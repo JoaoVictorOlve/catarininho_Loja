b9e7b2cd06039d22e5fd2c9d93a42ed63bb74535
Joao Victor De Oliveira <joao.oliveira@proway.treina>
2022-10-31 20:23:44 -0300

    Adicionado função para adicionar estoque de itens

--- controller.py
+++ controller.py
@@ -24,5 +24,43 @@ def apagarProduto(findProduto):
             for x in range(0, len(lista)): #Lê a lista indice por indice
                 arquivo.write(str(lista[x])+'\n') #Repõe os produtos da lista para dentro do arquivo de texto
 
-produto = input('Informe o produto: ')
-apagarProduto(produto)
+def aumentarEstoque(findProduto):
+    #Lista onde vão ser colocados os produtos dentro do arquivo
+    #   V
+    lista = []
+    encontrado = False
+
+    #Pega cada linha dentro do arquivo e joga dentro da lista
+    #   V
+    with open('produtos.txt', 'r') as arquivo: 
+        for numero, linha in enumerate(arquivo): #lê linha por linha o arquivo
+            relatorio = eval(linha) #converte a linha do arquivo para dicionário
+            lista.append(relatorio) #joga esse dicionário para dentro da lista
+    arquivo.close()
+        
+    #Altera a quantidade do produto dentro da lista e joga a lista de volta para o arquivo de texto
+    #   V
+    for x in range(0, len(lista)):
+        if findProduto == lista[x]['nome']:
+            encontrado = True
+            print('='*85)
+            print('|{:^20}|{:^20}|{:^20}|{:^20}'.format(' Nome', ' Tipo', ' Quantidade', ' Preço')) #Cabeçalho
+            print('='*85)
+            print('|{:^20}|{:^20}|{:^20}|{:^20}'.format(lista[x]['nome'],lista[x]['tipo'],lista[x]['quantidade'],lista[x]['preco']))
+            print('='*85)
+            while True:
+                aumentar = int(input('Informe a quantidade que quer aumentar\n>'))
+                lista[x]['quantidade'] = lista[x]['quantidade'] + aumentar
+                    
+                with open('produtos.txt', 'w') as arquivo:
+                    for x in range(0, len(lista)):
+                        arquivo.write(str(lista[x])+'\n')
+                
+                sair = input('Valor alterado!\nPressione enter para continuar')
+                break
+    if encontrado == False:
+        sair = input('Produto não encontrado!\nPressione enter para continuar')
+
+
+estoque = str(input('Informe o nome do produto que queira aumentar o estoque\n>'))
+aumentarEstoque(estoque)
